bc3f579705e15475f8b5a35443fcacf8725972e9
anantlk <anant.lk.1997@gmail.com>
2018-04-05 12:25:28 +0530

    Done

--- image_code.py
+++ image_code.py
@@ -6,24 +6,27 @@ import pyautogui
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_righteye_2splits.xml')
 
-video_capture = cv2.VideoCapture(0)
+video_capture = cv2.VideoCapture(0)  #object to access the camera
+
 c=0
+
 xcoord=0
 ycoord=0
-pyautogui.moveTo(pyautogui.size()[0]//2,pyautogui.size()[1]//2,duration=0.25)
-ymap=768//475
-xmap=1368//671
-wx,wy=pyautogui.position()
+
+pyautogui.moveTo(pyautogui.size()[0]//2,pyautogui.size()[1]//2,duration=0.25)  #Move the cursor to center of the screen
+
+
+wx,wy=pyautogui.position()   #get the mouse position
+
+
 while True:
-	#time.sleep(1)
-	ret, frame = video_capture.read()
+	ret, frame = video_capture.read()            #start reading the live stream using the camera
 
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 	for (x,y,w,h) in faces:
 		c=c+1
-		#cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 		roi_gray = gray[y:y+h, x:x+w]
 		roi_color = frame[y:y+h, x:x+w]
 		eyes = eye_cascade.detectMultiScale(roi_gray)
@@ -32,8 +35,8 @@ while True:
 		if(len(arr)>0):
 			print("x=",(x+w))
 			print("y=",(y+h))
-			
-			if(x-xcoord>0 and y-ycoord>0 and wx>0 and wy<768 ):
+			#conditions to move the cursor pointer
+			if(x-xcoord>0 and y-ycoord>0 and wx>0 and wy<768 ):               
 				wx=wx-35
 				wy=wy+25
 				pyautogui.moveTo(wx,wy,duration=0.01)
@@ -49,16 +52,16 @@ while True:
 				wx=wx+35
 				wy=wy-25
 				pyautogui.moveTo(wx,wy,duration=0.01)
-			#pyautogui.moveTo(wx,wy,duration=0.08)
 			xcoord=x
 			ycoord=y
 			print(pyautogui.position()[0],pyautogui.position()[1])
 		for (ex,ey,ew,eh) in eyes:
 			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-	# Display the resulting frame
 	cv2.namedWindow("new",cv2.WINDOW_NORMAL)
 	cv2.moveWindow("new",0,0)
 	frame=cv2.resize(frame,(1368,768))
+	
+	#take the screenchot of the screen and get the image of the area where pointer is moving 
 	if(c%2==0):
 		im=np.array(pyautogui.screenshot())
 		cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
@@ -69,7 +72,8 @@ while True:
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
 
-# When everything is done, release the capture
+
+#release the object
 video_capture.release()
 cv2.destroyAllWindows()
 
